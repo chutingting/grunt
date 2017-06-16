fd846bdfcd4cd253d83ddea3829fe4babbfd7743
chuting <lvsx@chutingdeMacBook-Air.local>
2017-06-16 17:37:45 +0800

    update project arch

---
+++ Gruntfile.js
@@ -0,0 +1,18 @@
+module.exports = function(grunt){
+    //配置
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '\n'
+            },
+            bulid: {
+                src: 'ueditor/ueditor.all.js',
+                dest: 'dest/ueditor.all.min.js'
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    //注册任务
+    grunt.registerTask('default', ['uglify']);
+};
